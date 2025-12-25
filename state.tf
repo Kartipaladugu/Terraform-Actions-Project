c6084c8da656845102d97b644f99bf961ee121cb
Suresh Paladugu <2reachsuresh@gmail.com>
2025-12-25 15:29:50 +0530

    backend

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
-# terraform {
-#   backend "gcs" {
-#     bucket = "terraform-state-bucket-2025"
-#     prefix = "terraform/state"
-#   }
-# }
+terraform {
+  backend "gcs" {
+    bucket = "terraform-state-bucket-2025"
+    prefix = "terraform/state"
+  }
+}
